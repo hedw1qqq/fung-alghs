cefdfe93ec523590ff005098937365b2271bbfd3
hedw1qqq <goga_glushko_igor@mail.ru>
2024-11-26 20:57:55 +0300

    make unique ptr

--- lab5/task7/main.cpp
+++ lab5/task7/main.cpp
@@ -40,32 +40,40 @@ private:
     double price;
     unsigned int storagePeriod;
 
-    void validate() const {
-        if (name.empty() || weight < 0 || price < 0) {
+    void validate(const std::string &name_, double weight_, double price_) const {
+        if (name_.empty() || weight_ < 0 || price_ < 0) {
             throw std::invalid_argument("Weight, price, and storage period must be non-negative.");
         }
     }
 
 public:
-    explicit Product(const std::string &name, unsigned int id, double weight, double price, unsigned int storagePeriod)
-            : name(name), id(id), weight(weight), price(price), storagePeriod(storagePeriod) {
-        validate();
+    explicit Product(const std::string &name, unsigned int id, double weight, double price,
+                     unsigned int storagePeriod) {
+        validate(name, weight, price);
+        this->name = name;
+        this->id = id;
+        this->weight = weight;
+        this->price = price;
+        this->storagePeriod = storagePeriod;
     }
 
-    Product(const Product &other)
-            : name(other.name), id(other.id), weight(other.weight), price(other.price),
-              storagePeriod(other.storagePeriod) {
-        validate();
+    Product(const Product &other) {
+        validate(other.name, other.weight, other.price);
+        this->name = other.name;
+        this->id = other.id;
+        this->weight = other.weight;
+        this->price = other.price;
+        this->storagePeriod = other.storagePeriod;;
     }
 
     Product &operator=(const Product &other) {
         if (this != &other) {
-            name = other.name;
-            id = other.id;
-            weight = other.weight;
-            price = other.price;
-            storagePeriod = other.storagePeriod;
-            validate();
+            validate(other.name, other.weight, other.price);
+            this->name = other.name;
+            this->id = other.id;
+            this->weight = other.weight;
+            this->price = other.price;
+            this->storagePeriod = other.storagePeriod;;
         }
         return *this;
     }
@@ -95,10 +103,12 @@ public:
     explicit PerishableProduct(const std::string &name, unsigned int id, double weight, double price,
                                unsigned int storagePeriod,
                                const std::string &expirationDateStr)
-            : Product(name, id, weight, price, storagePeriod), expirationDate(parseDate(expirationDateStr)) {}
+            : Product(name, id, weight, price, storagePeriod) {
+        this->expirationDate = parseDate(expirationDateStr);
+    }
 
     PerishableProduct(const PerishableProduct &other)
-            : Product(other), expirationDate((other.expirationDate)) {}
+            : Product(other), expirationDate(other.expirationDate) {}
 
     PerishableProduct &operator=(const PerishableProduct &other) {
         if (this != &other) {
@@ -146,26 +156,32 @@ private:
 public:
     explicit ElectronicProduct(const std::string &name, unsigned int id, double weight, double price, int storagePeriod,
                                const std::string &warrantyPeriod, double powerRating)
-            : Product(name, id, weight, price, storagePeriod),
-              warrantyPeriod(parseDate(warrantyPeriod)), powerRating(powerRating) {
+            : Product(name, id, weight, price, storagePeriod) {
         if (powerRating < 0) {
             throw std::invalid_argument("Power rating must be non-negative.");
         }
+        this->warrantyPeriod = parseDate(warrantyPeriod);
+        this->powerRating = powerRating;
 
     }
 
     std::string getCategory() const override { return "ElectronicProduct"; }
 
     ElectronicProduct(const ElectronicProduct &other)
-            : Product(other), warrantyPeriod(other.warrantyPeriod), powerRating(other.powerRating) {
+            : Product(other), warrantyPeriod(other.warrantyPeriod) {
         if (powerRating < 0) {
             throw std::invalid_argument("Power rating must be non-negative.");
         }
+        this->powerRating = other.powerRating;
     }
 
     ElectronicProduct &operator=(const ElectronicProduct &other) {
         if (this != &other) {
+            if (other.powerRating < 0) {
+                throw std::invalid_argument("Power rating must be non-negative.");
+            }
             Product::operator=(other);
+
             warrantyPeriod = other.warrantyPeriod;
             powerRating = other.powerRating;
         }
@@ -219,52 +235,49 @@ public:
 
 class WareHouse {
 private:
-    std::vector<Product *> products;
-
+    std::vector<std::unique_ptr<Product>> products;
 
 public:
     WareHouse() = default;
 
-
-    ~WareHouse() {
-        for (auto product: products) {
-            delete product;
-        }
-    }
-
-    WareHouse &operator+=(Product *product) {
+    WareHouse &operator+=(std::unique_ptr<Product> product) {
         auto it = std::find_if(products.begin(), products.end(),
-                               [product](Product *p) { return p->getID() == product->getID(); });
+                               [&product](const std::unique_ptr<Product> &p) {
+                                   return p->getID() == product->getID();
+                               });
         if (it != products.end()) {
             throw std::runtime_error("Product with this ID already exists in the warehouse.");
         }
-        products.push_back(product);
+        products.push_back(std::move(product));
         return *this;
     }
 
     WareHouse &operator-=(unsigned int id) {
         auto it = std::find_if(products.begin(), products.end(),
-                               [id](Product *p) { return p->getID() == id; });
+                               [id](const std::unique_ptr<Product> &p) { return p->getID() == id; });
         if (it == products.end()) {
             throw std::runtime_error("Product with this ID does not exist in the warehouse.");
         }
-        delete *it;
         products.erase(it);
         return *this;
     }
 
     Product *operator[](unsigned int id) const {
         auto it = std::find_if(products.begin(), products.end(),
-                               [id](Product *p) { return p->getID() == id; });
+                               [id](const std::unique_ptr<Product> &p) { return p->getID() == id; });
         if (it == products.end()) {
             return nullptr;
         }
-        return *it;
+        return it->get();
+    }
+
+    const std::vector<std::unique_ptr<Product>> &getProducts() const {
+        return products;
     }
 
     void displayAllProducts() {
         std::sort(products.begin(), products.end(),
-                  [](const Product *left, const Product *right) {
+                  [](const std::unique_ptr<Product> &left, const std::unique_ptr<Product> &right) {
                       return left->getCategory() < right->getCategory();
                   });
         for (const auto &product: products) {
@@ -281,13 +294,13 @@ public:
         return totalFee;
     }
 
-    std::vector<Product *> getExpiringProducts(const int days) const {
+    std::vector<Product *> getExpiringProducts(int days) const {
         std::vector<Product *> expiringProducts;
         time_t now = time(nullptr);
         time_t thresholdDate = now + days * 24 * 60 * 60;
 
         for (const auto &product: products) {
-            auto *perishable = dynamic_cast<PerishableProduct *>(product);
+            auto *perishable = dynamic_cast<PerishableProduct *>(product.get());
             if (perishable && perishable->isExpiringSoon(thresholdDate)) {
                 expiringProducts.push_back(perishable);
             }
@@ -295,27 +308,16 @@ public:
         return expiringProducts;
     }
 
-    std::vector<Product *> getProducts() const {
-        return products;
-    }
-
     std::vector<Product *> getCategoryProducts(const std::string &category) const {
         std::vector<Product *> answer;
-        for (auto *i: products) {
-            if (i->getCategory() == category) {
-                answer.push_back(i);
+        for (const auto &product: products) {
+            if (product->getCategory() == category) {
+                answer.push_back(product.get());
             }
         }
         return answer;
     }
-
 };
-
-
-std::ostream &operator<<(std::ostream &os, const Product &product) {
-    product.displayInfo();
-    return os;
-}
 
 std::ostream &operator<<(std::ostream &os, const WareHouse &wareHouse) {
     const auto &products = wareHouse.getProducts();
@@ -325,9 +327,9 @@ std::ostream &operator<<(std::ostream &os, const WareHouse &wareHouse) {
         return os;
     }
 
-    for (auto const product: products) {
+    for (const auto &product: products) {
         if (product != nullptr) {
-            os << *product;
+            os << product;
             os << "----------------------------------------\n";
         }
     }
@@ -336,30 +338,22 @@ std::ostream &operator<<(std::ostream &os, const WareHouse &wareHouse) {
 
 int main() {
     try {
+        PerishableProduct P("", 2, -2, -5, 14, "2024-12-05 10:00:00");
         WareHouse warehouse;
 
-        // Adding products manually
+        warehouse += std::make_unique<PerishableProduct>("Cheese", 2, 2.0, 5.0, 15, "2024-12-05 10:00:00");
+        warehouse += std::make_unique<ElectronicProduct>("Laptop", 3, 3.0, 1200.0, 30, "2025-01-01 00:00:00", 65.0);
+        warehouse += std::make_unique<PerishableProduct>("Milk", 1, 1.5, 2.5, 10, "2024-12-01 10:00:00");
+        warehouse += std::make_unique<ElectronicProduct>("Phone", 4, 1.0, 800.0, 20, "2025-01-15 00:00:00", 15.0);
+        warehouse += std::make_unique<BuildingMaterial>("Cement", 5, 50.0, 300.0, 60, false);
+        warehouse += std::make_unique<BuildingMaterial>("Paint", 6, 10.0, 50.0, 20, true);
 
-        warehouse += new PerishableProduct("Cheese", 2, 2.0, 5.0, 15, "2024-12-05 10:00:00");
-        warehouse += new ElectronicProduct("Laptop", 3, 3.0, 1200.0, 30, "2025-01-01 00:00:00", 65.0);
-        warehouse += new PerishableProduct("Milk", 1, 1.5, 2.5, 10, "2024-12-01 10:00:00");
-        warehouse += new ElectronicProduct("Phone", 4, 1.0, 800.0, 20, "2025-01-15 00:00:00", 15.0);
-        warehouse += new BuildingMaterial("Cement", 5, 50.0, 300.0, 60, false);
-        warehouse += new BuildingMaterial("Paint", 6, 10.0, 50.0, 20, true);
-        Product *product = warehouse[2];
-        if (product != nullptr) {
-            product->displayInfo();
-        } else {
-            std::cout << "Product with ID 2 not found.\n";
-        }
-
-        // Display all products
-        std::cout << "=== All Products ===\n";
+        // Display products
         warehouse.displayAllProducts();
 
         warehouse -= 5;
         warehouse -= 3;
-        // Display remaining products
+
         std::cout << "\n=== Remaining Products ===\n";
         warehouse.displayAllProducts();
 
@@ -367,19 +361,20 @@ int main() {
         double totalFee = warehouse.calculateTotalStorageFee();
         std::cout << "\nTotal storage fee: $" << totalFee << "\n";
 
-        // Find products expiring in the next 7 days
+        // Find expiring products
         int days = 7;
         auto expiringProducts = warehouse.getExpiringProducts(days);
 
         std::cout << "\n=== Products Expiring in the Next " << days << " Days ===\n";
-        for (const auto &i: expiringProducts) {
-            i->displayInfo();
+        for (const auto &product: expiringProducts) {
+            product->displayInfo();
             std::cout << "--------------------\n";
         }
 
-        std::cout << "Perishable products" << std::endl;
-        auto ans = warehouse.getCategoryProducts("PerishableProduct");
-        for (const auto &i: ans) {
+        // Get category products
+        std::cout << "\n=== Perishable Products ===\n";
+        auto perishableProducts = warehouse.getCategoryProducts("PerishableProduct");
+        for (const auto &product: perishableProducts) {
             product->displayInfo();
             std::cout << "--------------------\n";
         }
@@ -390,4 +385,5 @@ int main() {
 
     return 0;
 }
+
 
